386369d31eb51a06cc8f0c51caff4aff9477797b
wj <wanjian@zust.edu.cn>
2019-06-29 20:39:52 +0800

    通过初步测试

--- application/dataproc/process1.py
+++ application/dataproc/process1.py
@@ -9,15 +9,17 @@ import re
 
 
 def process1_1(process):
-    file_path = os.path.join(process['source_dir'][0], '18553.docx')
+    file_path = os.path.join(process['source_dir'][0], '30481.docx')
     doc = docx.Document(file_path)
     full_text = ''
     for para in doc.paragraphs:
         full_text = full_text + para.text + '\n'
-    print(full_text)
+
+    admission_record = dict()
+    progress_note = dict()
+    discharge_record = dict()
 
     table = doc.tables[0]
-    admission_record = dict()
     admission_record['姓名'] = table.cell(0, 1).text
     admission_record['职业'] = table.cell(0, 3).text
     admission_record['性别'] = table.cell(1, 1).text
@@ -32,12 +34,28 @@ def process1_1(process):
     admission_record['病史陈述者'] = table.cell(5, 3).text
 
     table = doc.tables[1]
-    discharge_record = {}
-    discharge_record['入院日期'] = table.cell(1, 1).text
-    discharge_record['出院日期'] = table.cell(1, 3).text
-    discharge_record['入院诊断'] = table.cell(2, 1).text
-    discharge_record['出院诊断'] = table.cell(2, 3).text
-    discharge_record['住院天数'] = table.cell(3, 1).text
+    i = 0 if len(table.rows) == 3 else 1
+    discharge_record['入院日期'] = table.cell(i, 1).text
+    discharge_record['出院日期'] = table.cell(i, 3).text
+    discharge_record['入院诊断'] = table.cell(i + 1, 1).text
+    discharge_record['出院诊断'] = table.cell(i + 1, 3).text
+    discharge_record['住院天数'] = table.cell(i + 2, 1).text
+
+    # 将full_text分成三部份admission_record_text、progress_note_text、discharge_record_text
+
+    regex='(入  院  记  录.*?)(首次病程记录|出  院  记  录)'
+    r = re.search(regex, full_text, re.S)
+    admission_record_text = r.group(1) if r is not None else ''
+
+    regex='(首次病程记录.*?)(入  院  记  录|出  院  记  录)'
+    r = re.search(regex, full_text, re.S)
+    progress_note_text = r.group(1) if r is not None else ''
+
+    regex='出  院  记  录.*'
+    r = re.search(regex, full_text, re.S)
+    discharge_record_text = r.group() if r is not None else ''
+
+    # 处理入院记录
 
     regex = '主诉\(Chief complaint\)：(?P<主诉>.+)\n' \
             '现病史（History of Present Illness）:(?P<现病史>.+)\n' \
@@ -54,25 +72,14 @@ def process1_1(process):
             '疾病相关评分:\n(?P<疾病相关评分>.+)\n' \
             '营养受损评分:\n(?P<营养受损评分>.+)\n' \
             '年龄评分:(?P<年龄评分>.+)\n' \
-            '营养风险评分:(?P<营养风险评分>.+)分  \(疾病有关评分\+营养受损评分\+年龄评分\)\n' \
+            '营养风险评分:(?P<营养风险评分>.+)分.*\n' \
             '是否请营养科会诊:(?P<是否请营养科会诊>.+)\n\n' \
             '功能评估:\(Function  Accessment\)\n((入院ADL评分:(?P<入院ADL评分>.+)分)|(入院ADL评分分级:(?P<入院ADL评分分级>.+)级)).*\n' \
             '是否请康复科会诊:(?P<是否请康复科会诊>.+)\n\n' \
             '心理评估\(Psychological Assessment\)\n护理入院心理评估是否阳性:(?P<护理入院心理评估是否阳性>.+)\n' \
-            '是否请心理卫生科会诊:(?P<是否请心理卫生科会诊>.+)\n.+首次病程记录.+\n' \
-            '病例特点： \n(?P<病例特点>.+)\n' \
-            '初步诊断：(?P<初步诊断1>.+)\n' \
-            '诊断依据：(?P<诊断依据>.+)\n' \
-            '鉴别诊断：(?P<鉴别诊断>.+)\n' \
-            '诊疗计划：\n(?P<诊疗计划>.+)\n.+(医师签名：|记录医生：).+出  院  记  录.+' \
-            '入院情况:(?P<入院情况>.+)\n' \
-            '住院经过:(?P<住院经过>.+)\n' \
-            '出院情况:(?P<出院情况>.+)\n' \
-            '出院医嘱:\n(?P<出院医嘱>.+)\n' \
-            '健康教育:(?P<健康教育>.+)\n' \
-            '随访计划:(?P<随访计划>.+)医师签名：'
+            '是否请心理卫生科会诊:(?P<是否请心理卫生科会诊>.+)\n'
 
-    match = re.search(regex, full_text, re.S)
+    match = re.search(regex, admission_record_text, re.S)
     a = match.groupdict()
     admission_record['主诉'] = a['主诉']
     admission_record['现病史'] = a['现病史']
@@ -108,8 +115,8 @@ def process1_1(process):
     r = re.search(regex, a['menstrual'], re.S)
     admission_record['月经史'] = r.group(1) if r is not None else ''
 
-    regex = '(初步诊断\(Diagnosis\).+)首次病程记录'
-    r = re.search(regex, full_text, re.S)
+    regex = '初步诊断\(Diagnosis\).*'
+    r = re.search(regex, admission_record_text, re.S)
     diagnosis = r.group()
 
     regex = '初步诊断\(Diagnosis\)：\n(.+?)医师签名：'
@@ -124,12 +131,34 @@ def process1_1(process):
     r = re.search(regex, diagnosis, re.S)
     admission_record['补充诊断'] = r.group(1) if r is not None else ''
 
-    pregress_note = dict()
-    pregress_note['病例特点'] = a['病例特点']
-    pregress_note['初步诊断'] = a['初步诊断1']
-    pregress_note['诊断依据'] = a['诊断依据']
-    pregress_note['鉴别诊断'] = a['鉴别诊断']
-    pregress_note['诊疗计划'] = a['诊疗计划']
+    # 处理首次病程记录
+
+    regex = '病例特点： \n(?P<病例特点>.+)\n' \
+            '初步诊断：(?P<初步诊断1>.+)\n' \
+            '诊断依据：(?P<诊断依据>.+)\n' \
+            '鉴别诊断：(?P<鉴别诊断>.+)\n' \
+            '诊疗计划：\n(?P<诊疗计划>.+)\n.+(医师签名：|记录医生：)'
+
+    match = re.search(regex, progress_note_text, re.S)
+    a = match.groupdict()
+
+    progress_note['病例特点'] = a['病例特点']
+    progress_note['初步诊断'] = a['初步诊断1']
+    progress_note['诊断依据'] = a['诊断依据']
+    progress_note['鉴别诊断'] = a['鉴别诊断']
+    progress_note['诊疗计划'] = a['诊疗计划']
+
+    # 处理出院记录
+
+    regex = '入院情况:(?P<入院情况>.+)\n' \
+            '住院经过:(?P<住院经过>.+)\n' \
+            '出院情况:(?P<出院情况>.+)\n' \
+            '出院医嘱(:|：)(?P<出院医嘱>.+)\n' \
+            '健康教育:(?P<健康教育>.+)\n' \
+            '随访计划:(?P<随访计划>.+)医师签名：'
+
+    match = re.search(regex, discharge_record_text, re.S)
+    a = match.groupdict()
 
     discharge_record['入院情况'] = a['入院情况']
     discharge_record['住院经过'] = a['住院经过']
@@ -138,9 +167,11 @@ def process1_1(process):
     discharge_record['健康教育'] = a['健康教育']
     discharge_record['随访计划'] = a['随访计划']
 
+    # 合并入院记录、首次病程记录、出院记录到medical_record中
+
     medical_record = dict()
     medical_record['入院记录'] = admission_record
-    medical_record['首次病程记录'] = pregress_note
+    medical_record['首次病程记录'] = progress_note
     medical_record['出院记录'] = discharge_record
 
     trim_dict_values(medical_record)
